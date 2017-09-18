66deb827233e45eda14b27ea09c49c894347846a
Matthew Gopez <gopezm@myumanitoba.ca>
2017-09-18 11:13:08 -0500

    Adding testing for fastq files, changing input of fasta file to match README documentation. Added Travis CI testing config file.

--- tests/test_fasta_subtyping.py
+++ tests/test_fasta_subtyping.py
@@ -1,14 +1,13 @@
 import pytest
 from pandas import DataFrame, Series
 import numpy as np
-
 from bio_hansel.subtype import Subtype
 from bio_hansel.subtyper import subtype_fasta
 
 
-@pytest.fixture
+@pytest.fixture()
 def test_genome():
-    return 'tests/data/FSIS1700719.fasta'
+    return 'tests/data/SRR1002850_SMALL.fasta'
 
 
 def test_fasta_subtyping(test_genome):
@@ -20,10 +19,14 @@ def test_fasta_subtyping(test_genome):
 
     assert st.scheme == scheme
     assert st.sample == genome_name
-    assert st.subtype == '2.2.1.1.1.1'
+    assert st.subtype == '2.2.2.2.1.4'
     assert st.inconsistent_subtypes is None
     assert st.n_tiles_matching_all == 202
     assert st.n_tiles_matching_all_total == '202'
+    assert st.n_tiles_matching_positive == 17
+    assert st.n_tiles_matching_positive_total == '17'
+    assert st.n_tiles_matching_subtype == 3
+    assert st.n_tiles_matching_subtype_total == '3'
 
     exp_cols = ['tilename', 'stitle', 'pident', 'length', 'mismatch', 'gapopen',
        'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore', 'qlen',
@@ -31,3 +34,4 @@ def test_fasta_subtyping(test_genome):
        'is_pos_tile', 'sample', 'file_path', 'scheme']
     df_cols = df.columns # type: Series
     assert np.all(df_cols.isin(exp_cols))
+
---
+++ tests/test_fastq_subtyping.py
@@ -0,0 +1,29 @@
+import pytest
+from pandas import DataFrame
+from bio_hansel.subtype import Subtype
+from bio_hansel.subtyper import subtype_reads
+
+
+@pytest.fixture
+def test_genome():
+    return 'tests/data/SRR5646583_SMALL.fastq'
+
+
+def test_fastq_subtyping(test_genome):
+    genome_name = 'test'
+    scheme = 'heidelberg'
+    st, df = subtype_reads(scheme='heidelberg', reads=test_genome, genome_name=genome_name, threads=4)
+    assert isinstance(st, Subtype)
+    assert isinstance(df, DataFrame)
+
+    assert st.scheme == scheme
+    assert st.sample == genome_name
+    assert st.subtype == '2.2.1.1.1.1'
+    assert st.are_subtypes_consistent is True
+    assert st.inconsistent_subtypes is None
+    assert st.n_tiles_matching_all == 202
+    assert st.n_tiles_matching_all_total == '202'
+    assert st.n_tiles_matching_positive == 20
+    assert st.n_tiles_matching_positive_total == '20'
+    assert st.n_tiles_matching_subtype == 2
+    assert st.n_tiles_matching_subtype_total == '2'
