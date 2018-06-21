355e337967718ec478ebbb382ebcc82581359f65
Gary Tong <gcttong@gmail.com>
2018-06-21 13:28:23 -0500

    handled exception for file downloads

--- bio_hansel/scripts/executeSnippy.py
+++ bio_hansel/scripts/executeSnippy.py
@@ -1,30 +1,36 @@
 import os
 from subprocess import call
+import shutil
 
 
-def executeSnippy(output_directory, reference_genome, inputgenomes):
+def executeSnippy(output_directory: str, reference_genome: str, inputgenomes: str):
+    """Runs the external Snippy command-line tool that will take the downloaded genomes and creat
 
-    
-    with open(inputgenomes,'r') as file:
-    #     for line in file:
-    #         line=line.rstrip()
-    #         call(["snippy", "--outdir",output_directory+"/mysnps"+line, "--ref",reference_genome, "--R1",output_directory+"/"+line+"_1.fastq.gz", "--R2", output_directory+"/"+line+"_2.fastq.gz"]) 
+    """
+    snippy_command="snippy1"
+    snippycore_command="snippy-core"
+    try:
+        if(shutil.which(snippy_command)) is not None:
         
+            with open(inputgenomes,'r') as file:
+                for line in file:
+                    line=line.rstrip()
+                    call(["snippy", "--outdir",f"{output_directory}/mysnps{line}", "--ref",reference_genome, "--R1",f"{output_directory}/{line}_1.fastq.gz", "--R2", f"{output_directory}/{line}_2.fastq.gz"]) 
+                
+                argument_list=["snippy-core", "--prefix", f"{output_directory}/core"]
 
-        argument_list=["snippy-core", "--prefix", output_directory+"/core"]
-
-        for line in file:
-            line=line.rstrip()
-            argument_list.append(output_directory+"/mysnps"+line)
+                for line in file:
+                    line=line.rstrip()
+                    argument_list.append(f"{output_directory}/mysnps{line}")
+                    
+                call(argument_list) 
+        else:
+            raise Exception("snippy not installed on this machine, cannot proceed any further")
             
+    except Exception as e:
+        print(e.args)
 
-
-        call(argument_list) 
-
-        return (f"{output_directory}/referencegroups.txt")
+    return (f"{output_directory}/referencegroups.txt")
 
         ##still need to use FastTree and then create a testreference.txt file
     
-
-if __name__ == '__main__':
-    executeSnippy()
---
+++ bio_hansel/scripts/testshutil.py
@@ -0,0 +1,6 @@
+import shutil
+
+command = 'fdsdfsd'
+if (shutil.which(command) is not None):
+    print('person')
+
