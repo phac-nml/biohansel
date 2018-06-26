578aa4c0c03e3eede8480efe2a53001546c6b09e
Gary Tong <gcttong@gmail.com>
2018-06-26 16:13:43 -0500

    removed files

--- bio_hansel/scripts/print.sh
+++
@@ -1,3 +0,0 @@
-feht -i testreference.txt -d fishertest.txt --one "group A" -f 1 >feht_resultsA.txt 
-feht -i testreference.txt -d fishertest.txt --one "group B" -f 1 >feht_resultsB.txt 
-feht -i testreference.txt -d fishertest.txt --one "group C" -f 1 >feht_resultsC.txt 
