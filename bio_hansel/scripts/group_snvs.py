d4ff06c2aa61ff75f5a69525be57c60a0f04da2d
Gary Tong <gcttong@gmail.com>
2018-07-18 14:26:33 -0500

    modifed read_vcf that splits up the two dataframes into binary data and snv data

--- bio_hansel/scripts/group_snvs.py
+++ bio_hansel/scripts/group_snvs.py
@@ -3,21 +3,20 @@ from typing import Dict
 import pandas as pd
 
 
-def fisher_test(modified_df: pd.DataFrame,
+def group_snvs(binary_df: pd.DataFrame, sequence_df: pd.DataFrame
                 test_groups: Dict[str, str]) -> Dict[str, pd.DataFrame]:
     """Takes in a DataFrame containing SNV VCF data and extracts the SNVs that are specific to a group, and only to that
     group
 
     Args:
-        modified_df: a DataFrame containing SNV VCF data
-        test_groups: the group of SNVs that are to be included for testing
-
+        binary_df: the DataFrame that contains the binary SNV data
+        sequence_df: the DataFrame that contains just the REF/ALT sequence info
+        groups_dict: the dictionary that contains the group information for each genome
     Returns:
         results_list: A dictionary containing the group allocation and DataFrame of SNVs that are associated with that
                       group
     """
-    attributes = modified_df[['POS', 'REF', 'ALT']]
-    snvs_only = modified_df.drop(['POS', 'REF', 'ALT'], axis=1)
+   
     unique_groups = list(set(test_groups.values()))
     results_list = {}
     other_list = []
@@ -28,15 +27,15 @@ def fisher_test(modified_df: pd.DataFrame,
                 current_list.append(key)
             else:
                 other_list.append(key)
-        dfsnv_curr = snvs_only[current_list]
-        dfsnv_other = snvs_only[other_list]
+        dfsnv_curr = binary_df[current_list]
+        dfsnv_other = binary_df[other_list]
         row_sums_curr = dfsnv_curr.sum(axis=1)
         row_sums_other = dfsnv_other.sum(axis=1)
         new_data_frame = (dfsnv_curr.loc[(
                                                  (row_sums_curr == 0) & (row_sums_other == len(other_list))
                                          ) | ((row_sums_curr == len(current_list)) & (row_sums_other == 0)), :])
-        final_table = pd.concat([attributes, new_data_frame], axis=1)
-        final_table = final_table[final_table.columns[:4]]
+        final_table = pd.concat([sequence_df, new_data_frame], axis=1)
+        final_table = final_table[final_table.columns[:3]]
         results_list[x] = final_table.dropna()
         current_list = []
         other_list = []
