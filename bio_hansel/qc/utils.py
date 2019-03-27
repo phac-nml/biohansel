34750dddfe47a275b2111723c65412b49e77f53a
Darian Hole <darianhole@gmail.com>
2019-03-27 09:24:29 -0500

    changed 'tile' to 'kmer' to create consistency

--- bio_hansel/qc/utils.py
+++ bio_hansel/qc/utils.py
@@ -5,47 +5,47 @@ from pandas import DataFrame
 from ..subtype import Subtype
 
 
-def get_conflicting_tiles(st: Subtype, df: DataFrame) -> Optional[DataFrame]:
-    """ Get positive and negative tiles that both are present for a subtype.
+def get_conflicting_kmers(st: Subtype, df: DataFrame) -> Optional[DataFrame]:
+    """ Get positive and negative kmers that both are present for a subtype.
 
-    Find positive and negative tiles for the same refposition/target site in the results `df`.
+    Find positive and negative kmers for the same refposition/target site in the results `df`.
 
     Args:
         st: Subtype results summary
         df: Detailed subtyping results
 
     Returns:
-        DataFrame of conflicting positive and negative tiles
+        DataFrame of conflicting positive and negative kmers
     """
     if st.is_fastq_input():
         dfst = df[(df['subtype'] == str(st.subtype)) & (df['is_kmer_freq_okay'])]
     else:  # fasta files
         dfst = df[(df['subtype'] == str(st.subtype))]
 
-    pos_tile_positions = dfst[dfst['is_pos_tile']]['refposition']
-    neg_tiles = dfst[~dfst['is_pos_tile']]
-    return neg_tiles[neg_tiles['refposition'].isin(pos_tile_positions)]
+    pos_kmer_positions = dfst[dfst['is_pos_kmer']]['refposition']
+    neg_kmers = dfst[~dfst['is_pos_kmer']]
+    return neg_kmers[neg_kmers['refposition'].isin(pos_kmer_positions)]
 
 
-def get_num_pos_neg_tiles(st: Subtype, df: DataFrame) -> Tuple[int, int]:
-    """Get the number of positive and negative tiles for a given subtype
+def get_num_pos_neg_kmers(st: Subtype, df: DataFrame) -> Tuple[int, int]:
+    """Get the number of positive and negative kmers for a given subtype
 
     Args:
         st: Subtype results summary
         df: Detailed subtyping results
 
     Returns:
-        (number of positive tiles, number of negative tiles)
+        (number of positive kmers, number of negative kmers)
     """
     dfst = df[(df['subtype'] == str(st.subtype))]
-    return dfst[dfst['is_pos_tile']].shape[0], dfst[~dfst['is_pos_tile']].shape[0]
+    return dfst[dfst['is_pos_kmer']].shape[0], dfst[~dfst['is_pos_kmer']].shape[0]
 
 
-def get_mixed_subtype_tile_counts(dfpos: DataFrame, subtype_list: List[Any]) -> Dict[str, int]:
-    st_pos_tiles = {}
+def get_mixed_subtype_kmer_counts(dfpos: DataFrame, subtype_list: List[Any]) -> Dict[str, int]:
+    st_pos_kmers = {}
 
     for st in subtype_list:
         bs = dfpos.subtype == [st[:x] for x in dfpos.subtype.str.len()]
-        st_pos_tiles[st] = bs.sum()
+        st_pos_kmers[st] = bs.sum()
 
-    return st_pos_tiles
+    return st_pos_kmers
