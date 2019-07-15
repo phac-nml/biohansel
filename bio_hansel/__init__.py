516251d4f40d10f41bbdc55b1499b5c39d83ff68
Darian Hole <darianhole@gmail.com>
2019-07-15 13:26:20 -0500

    update tb + typhi schemes

--- bio_hansel/__init__.py
+++ bio_hansel/__init__.py
@@ -4,6 +4,6 @@ __version__ = '2.2.0'
 program_name = 'bio_hansel'
 program_summary = 'Subtype microbial genomes using SNV targeting k-mer subtyping schemes.'
 program_desc = program_summary + '''
-Includes schemes for Salmonella enterica spp. enterica serovar Heidelberg, Enteritidis, and Typhi subtyping. Also includes a Mycobacterium Tuberculosis scheme. 
+Includes schemes for Salmonella enterica spp. enterica serovar Heidelberg, Enteritidis, Typhi, and Typhimurium subtyping. Also includes a Mycobacterium Tuberculosis scheme. 
 Developed by Geneviève Labbé, James Robertson, Peter Kruczkiewicz, Marisa Rankin, Matthew Gopez, Chad R. Laing, Philip Mabon, Kim Ziebell, Aleisha R. Reimer, Lorelee Tschetter, Gary Van Domselaar, Sadjia Bekal, Kimberley A. MacDonald, Linda Hoang, Linda Chui, Danielle Daignault, Durda Slavic, Frank Pollari, E. Jane Parmley, David Son, Darian Hole, Philip Mabon, Elissa Giang, Lok Kan Lee, Jonathan Moffat, Marisa Rankin, Joanne MacKinnon, Roger Johnson, John H.E. Nash.
 '''
