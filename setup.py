27accbab82e0a12e43db8333734adf71b61a9e96
Peter Kruczkiewicz <peter.kruczkiewicz@gmail.com>
2021-03-03 12:42:24 -0600

    Bump version: 2.5.0 → 2.5.1

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/phac-nml/biohansel',
-    version='2.5.0',
+    version='2.5.1',
     zip_safe=False,
 )
